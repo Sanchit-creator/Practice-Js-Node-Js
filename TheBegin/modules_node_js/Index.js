87e753f9bd44b30f39aea01ff8e58ac7c95c786c
Sanchit98 <sanchit@novolytics.co>
2022-07-08 11:54:10 +0530

    first commit

---
+++ TheBegin/modules_node_js/Index.js
@@ -0,0 +1,11 @@
+// Old method
+
+const operations = require('./Operations');
+
+console.log(operations.add(2, 3));
+
+// Es 6 method 
+
+// import  * as name from './Operations';
+
+// console.log(name.add(2, 3));
---
+++ TheBegin/modules_node_js/Operations.js
@@ -0,0 +1,14 @@
+// module:this whole file is a module
+// module.eport is an object giving add as a key; and can use  subtract, multiply, division
+
+
+// Old method
+module.exports.add = function(a, b) {
+    return a + b;
+}
+
+
+// New ES 6
+// export function add(a, b) {
+//     return a + b;
+// }
